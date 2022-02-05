0bcea11197e9f1e893c294748b4446bf589b80ac
Karen Nikoghosyan <karen.nikoghosyan11@gmail.com>
2022-02-05 20:31:14 +0200

    V.1.0.7.4
    -Intro screen refactoring

---
+++ MusicDB/Models/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  MusicDB
+//
+//  Created by Karen Nikoghosyan on 05/02/2022.
+//
+
+import UIKit
+
+struct Constants {
+    
+    struct Fonts {
+        static let futuraTitle = UIFont(name: "Futura-Bold", size: 21.0)!
+        static let futuraDescription = UIFont(name: "Futura", size: 17.0)!
+    }
+}
